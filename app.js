485505744b7742e166d2c8a2ba8e5d811b41bbff
greenteawarrior <emily.wang@students.olin.edu>
2014-01-13 03:07:28 -0500

    working logins! woohoo

--- app.js
+++ app.js
@@ -27,6 +27,25 @@ app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+//mongoose!!
+//refer to https://devcenter.heroku.com/articles/nodejs-mongoose
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/HelloMongoose';
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+// //end of mongoose stuff in app.js
+
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
--- routes/twitterclone.js
+++ routes/twitterclone.js
@@ -11,20 +11,34 @@ exports.newuser_post = function(req, res){
   //login stuff with sessions
   var username = req.body.username;
   console.log(username);
-  var user = new models.User({username: username});
-  
-  user.save(function (err) {
-    if (err) 
-      console.log("Problem saving new user", err);
+  var existingU = models.User.find({'username': username}).exec(function(err, existingU){
+    if (err) {
+      console.log(err);
+    }
+    else if (existingU.length == 0) {
+      //length 0 implies user w/ that username does not exist yet
+      var user = new models.User({username: username});
+      user.save(function (err) {
+        if (err)
+          console.log("Problem saving new user", err);
+      });
+      req.session.user = user;
+      res.redirect("/");
+    }
+    else {
+      console.log("user already exists");
+      res.redirect("/");
+    }
   });
-
-  req.session.user = user;
-
-  res.redirect('/');
 };
 
 //GET /
 exports.index = function(req, res){
   var currentUser = req.session.user;
-  res.render('index', { title: 'Express', username: currentUser.username});
+  if (currentUser) {
+    res.render('index', { title: 'Express', username: currentUser.username});
+  }
+  else {
+    res.render('index', { title: 'Express', username: "nobody yet"});    
+  }
 };
