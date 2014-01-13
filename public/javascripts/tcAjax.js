8c946002a7746c97a99177810e6144dffec64e8e
greenteawarrior <emily.wang@students.olin.edu>
2014-01-13 06:37:30 -0500

    setting up ajax stuff

---
+++ public/javascripts/tcAjax.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  //use setInterval to poll for new tweets every 2 seconds
+
+  $(".tweetButton").click(function() {
+    //check to see if the tweet is <140 characters, then act accordingly
+    
+    //if it's <140 characters, display error message and do not do post request yet
+    
+    //else if it's a legit tweet,
+    //turn the error message variable into an empty string
+    //manipulate html to put the tweet on the page
+    //post request
+  });
+});
