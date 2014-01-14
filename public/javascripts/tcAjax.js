d735019d1f90c41c55500c25c712af0e86d73912
greenteawarrior <emily.wang@students.olin.edu>
2014-01-14 01:31:18 -0500

    jquery stuff works! yay \o/

--- public/javascripts/tcAjax.js
+++ public/javascripts/tcAjax.js
@@ -4,10 +4,11 @@ $(document).ready(function() {
   var index = 0; //initialize
   //perhaps in another iteration of this website - index starts at a nonzero number (since it's silly to get tweets from the absolute beginning all the time,
   //perhaps just do the tweets from that specific day? tinker with the object properties)  
+  //in next iteration, play with the .show jquery function
   
   var streaming = function() {
     $.get("/tweets/partial/" + index, function(obtainedTweet) {
-      $("ul").prepend(obtainedTweet);
+      $(".tweetstream").prepend(obtainedTweet);
       index += 1;
     });
   };
@@ -15,10 +16,18 @@ $(document).ready(function() {
   //use setInterval to poll for new tweets every 2 seconds
   setInterval(streaming, 2000); 
 
-  $('.tweetButton').click(function() {
-      $("h1").append('Ahh!');
-      return false;
+  $('#tweetform').on('submit', function() {
+    var message = $('#message').val();
+    var messageLength = message.length;
+    if (messageLength < 141) {
+      $.post("/tweets", $(this).serialize());
+      });
+    }
+    else {
+      alert("Your current input is " + messageLength + " characters long, which is more than the 140char limit. Please fix it and try submitting again. :)")
+    }
+    return false
   });
-
   return false
+
 });
